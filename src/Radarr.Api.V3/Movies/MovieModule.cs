3c1fb9899f5a02b296e46e28f675a3f7d89162ec
geogolem <geogolem@gmail.com>
2020-09-04 14:44:49 -0400

    When deleting movies, the MovieFileDeletion event was not triggered even if delete all movie files option was selected

--- src/Radarr.Api.V3/Movies/MovieModule.cs
+++ src/Radarr.Api.V3/Movies/MovieModule.cs
@@ -251,7 +251,7 @@ namespace Radarr.Api.V3.Movies
 
         public void Handle(MovieFileDeletedEvent message)
         {
-            if (message.Reason == DeleteMediaFileReason.Upgrade)
+            if (message.Reason == DeleteMediaFileReason.Upgrade || message.Reason == DeleteMediaFileReason.MovieDeletion)
             {
                 return;
             }
